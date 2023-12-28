8231a9102dd91f4002e5cddf8a4dbd334182816d
Victor Tsang <victorwltsang@gmail.com>
2023-12-28 09:28:12 -0800

    feat(input): use color tokens

--- packages/input/CHANGELOG.md
+++ packages/input/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @vktrwlt/input
 
+## 1.1.0
+
+### Minor Changes
+
+- use color token
+
 ## 1.0.1
 
 ### Patch Changes
--- packages/input/src/Input.js
+++ packages/input/src/Input.js
@@ -1,21 +1,22 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { colors } from "@vktrwlt/design-tokens";
 
 export const Input = styled.input`
-    border-radius: 8px;
-    border: 2px solid #0f6;
+  border-radius: 8px;
+  border: 2px solid;
+  border-color: ${colors.red};
+  font-size: 16px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
 
-    font-size: 16px;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  padding: 16px 16px;
 
-    padding: 16px 16px;
+  width: 200px;
 
-    width: 200px;
+  &:active {
+    outline: none;
+  }
 
-    &:active{
-        outline: none;
-    }
-
-    &:focus{
-        outline: none;
-    }
-`
+  &:focus {
+    outline: none;
+  }
+`;
